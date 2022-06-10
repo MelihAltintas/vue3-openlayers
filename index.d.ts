bce26faf3d07526e72026f03d82e1e3fbb2bd983
Pavel Zhytomirsky <r3volut1oner@gmail.com>
2022-06-10 14:45:07 +0300

    Typescript types declarations (#82)

---
+++ index.d.ts
@@ -0,0 +1,22 @@
+declare module 'vue3-openlayers' {
+  import {App} from 'vue';
+  import feature from 'ol/Feature';
+  import * as geom from 'ol/geom/';
+  import * as format from 'ol/format';
+  import * as loadingstrategy from 'ol/loadingstrategy';
+  import * as selectconditions from 'ol/events/condition';
+  import * as extent from 'ol/extent';
+  import * as animations from 'ol/easing';
+
+  module '@vue/runtime-core' {
+    export declare function inject(key: 'ol-feature'): typeof feature
+    export declare function inject(key: 'ol-geom'): typeof geom
+    export declare function inject(key: 'ol-animations'): typeof animations
+    export declare function inject(key: 'ol-format'): typeof format
+    export declare function inject(key: 'ol-loadingstrategy'): typeof loadingstrategy
+    export declare function inject(key: 'ol-selectconditions'): typeof selectconditions
+    export declare function inject(key: 'ol-extent'): typeof extent
+  }
+
+  export default function install(app: App): void
+}
