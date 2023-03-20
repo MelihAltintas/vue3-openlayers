8866adbc9d51a5fd80daf7bd43ab069e238d2602
ikolevatykh <kolevatykh.ilja@gmail.com>
2023-03-21 00:59:36 +0300

    add components declaration

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'url'
 import dts from 'vite-plugin-dts'
+import Components from 'unplugin-vue-components/vite'
 
 export default defineConfig({
   plugins: [
     vue(),
+    Components({
+      dts: true,
+    }),
     dts({
       outputDir: 'dist/types',
       insertTypesEntry: true,
