a123e83272c1539631af5e4d36d0806ee3148ddf
Danny Koppenhagen <Danny.Koppenhagen@deutschebahn.com>
2023-03-28 22:12:28 +0200

    chore: remove unneeded babel conf file

--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,36 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import {resolve} from "path"
+import { fileURLToPath, URL } from "url";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      "@": resolve(__dirname, "./src"),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
     preserveSymlinks: false,
-    dedupe: ['vue'],
-    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
+    dedupe: ["vue"],
+    extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue"],
   },
   build: {
     lib: {
-      entry: resolve(__dirname, './src/index.js'),
-      name: 'vue3-openlayers',
+      entry: fileURLToPath(new URL("./src/index.js", import.meta.url)),
+      name: "vue3-openlayers",
 
       fileName: (format) => `vue3-openlayers.${format}.js`,
-
     },
     rollupOptions: {
-      external: ['vue'],
+      external: ["vue"],
       output: {
-        inlineDynamicImports : true,
+        inlineDynamicImports: true,
         globals: {
-          vue: 'Vue'
+          vue: "Vue",
         },
         assetFileNames: (assetInfo) => {
-          if (assetInfo.name == 'style.css')
-            return 'vue3-openlayers.css';
+          if (assetInfo.name == "style.css") return "vue3-openlayers.css";
           return assetInfo.name;
         },
-      }
-    }
-  }
-})
+      },
+    },
+  },
+});
