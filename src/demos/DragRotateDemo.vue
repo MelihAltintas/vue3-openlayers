a1b211bc9f0dae9341c9a6bd9130162eb6b9bd13
Danny Koppenhagen <Danny.Koppenhagen@deutschebahn.com>
2023-03-29 23:28:29 +0200

    feat: add interaction dragrotate
    closes #129

---
+++ src/demos/DragRotateDemo.vue
@@ -0,0 +1,37 @@
+<template>
+  <ol-map
+    :loadTilesWhileAnimating="true"
+    :loadTilesWhileInteracting="true"
+    style="height: 400px"
+  >
+    <ol-view
+      ref="view"
+      :center="center"
+      :zoom="zoom"
+      :projection="projection"
+    />
+
+    <ol-tile-layer>
+      <ol-source-osm />
+    </ol-tile-layer>
+
+    <ol-interaction-dragrotate />
+  </ol-map>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  setup() {
+    const center = ref([-102.13121, 40.2436]);
+    const projection = ref("EPSG:4326");
+    const zoom = ref(5);
+
+    return {
+      center,
+      projection,
+      zoom,
+    };
+  },
+};
+</script>
