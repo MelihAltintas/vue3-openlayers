60d0fa4b9b0405da7c16d26aa16f53f0d982c0fd
Danny Koppenhagen <Danny.Koppenhagen@deutschebahn.com>
2023-05-29 11:23:43 +0200

    refactor(ol-animation-path): migrate to script setup

---
+++ src/components/animations/OlAnimationPath.vue
@@ -0,0 +1,34 @@
+<template lang="">
+  <slot></slot>
+</template>
+
+<script setup>
+import Path from "ol-ext/featureanimation/Path";
+import useAnimation from "@/composables/useAnimation";
+import useAnimationProps from "@/composables/useAnimationProps";
+
+const props = defineProps({
+  ...useAnimationProps(),
+  rotate: {
+    type: Boolean,
+    default: false,
+  },
+  speed: {
+    type: Number,
+    default: 0,
+  },
+  path: {
+    type: Object,
+  },
+});
+
+const { map, vectorLayer, properties } = useAnimation(Path, props);
+
+defineExpose({
+  map,
+  vectorLayer,
+  properties,
+});
+</script>
+
+<style lang=""></style>
--- src/components/animations/index.js
+++ src/components/animations/index.js
@@ -1,11 +1,11 @@
 import OLAnimationDrop from "./OLAnimationDrop.vue";
 import OlAnimationFade from "./OlAnimationFade.vue";
 import OlAnimationFeature from "./OlAnimationFeature.vue";
+import OlAnimationPath from "./OlAnimationPath.vue";
 import ShakeAnimation from "./ShakeAnimation.vue";
 import ZoomAnimation from "./ZoomAnimation.vue";
 import TeleportAnimation from "./TeleportAnimation.vue";
 import SlideAnimation from "./SlideAnimation.vue";
-import PathAnimation from "./PathAnimation.vue";
 
 function install(app) {
   if (install.installed) {
@@ -17,11 +17,11 @@ function install(app) {
   app.component("ol-animation-drop", OLAnimationDrop);
   app.component("ol-animation-fade", OlAnimationFade);
   app.component("ol-animation-feature", OlAnimationFeature);
+  app.component("ol-animation-path", OlAnimationPath);
   app.component(ShakeAnimation.name, ShakeAnimation);
   app.component(ZoomAnimation.name, ZoomAnimation);
   app.component(TeleportAnimation.name, TeleportAnimation);
   app.component(SlideAnimation.name, SlideAnimation);
-  app.component(PathAnimation.name, PathAnimation);
 }
 
 export default install;
@@ -31,9 +31,9 @@ export {
   OLAnimationDrop,
   OlAnimationFade,
   OlAnimationFeature,
+  OlAnimationPath,
   ShakeAnimation,
   ZoomAnimation,
   TeleportAnimation,
-  PathAnimation,
   SlideAnimation,
 };
