846c0ed97c83a655999029469a885f9f31a17d3d
Danny Koppenhagen <Danny.Koppenhagen@deutschebahn.com>
2023-05-29 11:39:23 +0200

    refactor(ol-animation-shake): migrate to script setup

---
+++ src/components/animations/OlAnimationShake.vue
@@ -0,0 +1,36 @@
+<template lang="">
+  <slot></slot>
+</template>
+
+<script setup>
+import Shake from "ol-ext/featureanimation/Shake";
+import useAnimation from "@/composables/useAnimation";
+import useAnimationProps from "@/composables/useAnimationProps";
+
+const props = defineProps({
+  ...useAnimationProps(),
+  bounce: {
+    type: Number,
+    default: 6,
+  },
+  amplitude: {
+    type: Number,
+    default: 40,
+  },
+  horizontal: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const { map, vectorLayer, properties, animation } = useAnimation(Shake, props);
+
+defineExpose({
+  map,
+  vectorLayer,
+  properties,
+  animation,
+});
+</script>
+
+<style lang=""></style>
--- src/components/animations/index.js
+++ src/components/animations/index.js
@@ -2,7 +2,7 @@ import OLAnimationDrop from "./OLAnimationDrop.vue";
 import OlAnimationFade from "./OlAnimationFade.vue";
 import OlAnimationFeature from "./OlAnimationFeature.vue";
 import OlAnimationPath from "./OlAnimationPath.vue";
-import ShakeAnimation from "./ShakeAnimation.vue";
+import OlAnimationShake from "./OlAnimationShake.vue";
 import ZoomAnimation from "./ZoomAnimation.vue";
 import TeleportAnimation from "./TeleportAnimation.vue";
 import SlideAnimation from "./SlideAnimation.vue";
@@ -18,7 +18,7 @@ function install(app) {
   app.component("ol-animation-fade", OlAnimationFade);
   app.component("ol-animation-feature", OlAnimationFeature);
   app.component("ol-animation-path", OlAnimationPath);
-  app.component(ShakeAnimation.name, ShakeAnimation);
+  app.component("ol-animation-shake", OlAnimationShake);
   app.component(ZoomAnimation.name, ZoomAnimation);
   app.component(TeleportAnimation.name, TeleportAnimation);
   app.component(SlideAnimation.name, SlideAnimation);
@@ -32,7 +32,7 @@ export {
   OlAnimationFade,
   OlAnimationFeature,
   OlAnimationPath,
-  ShakeAnimation,
+  OlAnimationShake,
   ZoomAnimation,
   TeleportAnimation,
   SlideAnimation,
