eb61b9f0a8a2bada54746f96d8bde9febe885467
Danny Koppenhagen <Danny.Koppenhagen@deutschebahn.com>
2023-06-12 21:55:25 +0200

    fix(ol-heatmap-layer): use correct prop types and support weight prop as string

--- src/components/layers/OlHeatmapLayer.vue
+++ src/components/layers/OlHeatmapLayer.vue
@@ -15,17 +15,25 @@ import {
   type LayersCommonProps,
 } from "@/components/layers/LayersCommonProps";
 
+type WeightFunction = () => number;
+
 const props = withDefaults(
   defineProps<
     LayersCommonProps & {
-      weight?: () => number;
+      weight?: string | WeightFunction;
       extent?: Extent;
       blur?: number;
       radius?: number;
-      gradient?: number[];
+      gradient?: string[];
     }
   >(),
-  layersCommonDefaultProps
+  {
+    ...layersCommonDefaultProps,
+    weight: "weight",
+    radius: 8,
+    blur: 15,
+    gradient: () => ["#00f", "#0ff", "#0f0", "#ff0", "#f00"],
+  }
 );
 
 const map = inject<Map>("map");
