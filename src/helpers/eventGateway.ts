0ad2a05a83acc51e823dcc0a5e587a547e90469c
Danny Koppenhagen <Danny.Koppenhagen@deutschebahn.com>
2023-06-16 11:24:10 +0200

    feat: provide eventGateway to pass-through OpenLayers events and emit them on a component

---
+++ src/helpers/eventGateway.ts
@@ -0,0 +1,13 @@
+import type { defineEmits } from "vue";
+
+export default function eventGateway(
+  emit: ReturnType<typeof defineEmits>,
+  instance: { on: (event: any, ...args: any[]) => unknown },
+  events: string[]
+) {
+  events.forEach((event: string) => {
+    instance.on(event, (...args: unknown[]) => {
+      emit(event, ...args);
+    });
+  });
+}
