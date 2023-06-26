e6dca45ed48fbf9627de9c96b3645ece9f4c098f
Danny Koppenhagen <Danny.Koppenhagen@deutschebahn.com>
2023-06-26 10:45:03 +0200

    refactor(ol-source-vector): pass-through all props and events from `ol/source/Vector`
    BREAKING CHANGE:
    - use default values from OpenLayers version latest (7.4.0)

--- src/components/sources/OlSourceVector.vue
+++ src/components/sources/OlSourceVector.vue
@@ -5,54 +5,23 @@
 </template>
 
 <script setup lang="ts">
-import type { LoadingStrategy, Options } from "ol/source/Vector";
-import VectorSource from "ol/source/Vector";
+import VectorSource, { type Options } from "ol/source/Vector";
 import type VectorLayer from "ol/layer/Vector";
 import type HeatmapLayer from "ol/layer/Heatmap";
 import type { Ref } from "vue";
 import { inject, watch, onMounted, onUnmounted, provide, computed } from "vue";
-import type { AttributionLike } from "ol/source/Source";
-import type Collection from "ol/Collection";
-import type Feature from "ol/Feature";
 import type Geometry from "ol/geom/Geometry";
-import type FeatureFormat from "ol/format/Feature";
-import type { FeatureLoader, FeatureUrlFunction } from "ol/featureloader";
 import usePropsAsObjectProperties from "@/composables/usePropsAsObjectProperties";
-import type { ProjectionLike } from "ol/proj";
+import eventGateway, { FEATURE_EVENTS } from "@/helpers/eventGateway";
 
-const props = withDefaults(
-  defineProps<{
-    attributions?: AttributionLike;
-    features?: Collection<Feature<Geometry>>;
-    format?: FeatureFormat | undefined;
-    loader?: FeatureLoader;
-    overlaps?: boolean;
-    projection?: ProjectionLike;
-    strategy?: LoadingStrategy;
-    url?: string | FeatureUrlFunction;
-    useSpatialIndex?: boolean;
-    wrapX?: boolean;
-  }>(),
-  {
-    overlaps: true,
-    projection: "EPSG:3857",
-    useSpatialIndex: true,
-    wrapX: true,
-  }
-);
+const props = withDefaults(defineProps<Options>(), {
+  overlaps: true,
+  projection: "EPSG:3857",
+  useSpatialIndex: true,
+  wrapX: true,
+});
 
-const emit = defineEmits([
-  "addfeature",
-  "change",
-  "changefeature",
-  "clear",
-  "error",
-  "featuresloadend",
-  "featuresloaderror",
-  "featuresloadstart",
-  "propertychange",
-  "removefeature",
-]);
+const emit = defineEmits([]);
 
 const vectorLayer = inject<Ref<VectorLayer<VectorSource>> | null>(
   "vectorLayer",
@@ -66,24 +35,13 @@ const { properties } = usePropsAsObjectProperties(props);
 const source = computed(() => {
   const vs = new VectorSource(properties as Options<Geometry>);
 
-  vs.on("addfeature", (...args) => emit("addfeature", ...args));
-  vs.on("change", (...args) => emit("change", ...args));
-  vs.on("changefeature", (...args) => emit("changefeature", ...args));
-  vs.on("clear", (...args) => emit("clear", ...args));
-  vs.on("error", (...args) => emit("error", ...args));
-  vs.on("featuresloadend", (...args) => emit("featuresloadend", ...args));
-  vs.on("featuresloaderror", (...args) => emit("featuresloaderror", ...args));
-  vs.on("featuresloadstart", (...args) => emit("featuresloadstart", ...args));
-  vs.on("propertychange", (...args) => emit("propertychange", ...args));
-  vs.on("removefeature", (...args) => emit("removefeature", ...args));
+  eventGateway(emit, vs, FEATURE_EVENTS);
 
   return vs;
 });
 
 const applySource = () => {
-  // @ts-ignore
   layer?.value?.setSource(null);
-  // @ts-ignore
   layer?.value?.setSource(source.value);
   layer?.value?.changed();
 };
@@ -99,12 +57,10 @@ watch(
 );
 
 onMounted(() => {
-  // @ts-ignore
   layer?.value?.setSource(source.value);
 });
 
 onUnmounted(() => {
-  // @ts-ignore
   layer?.value?.setSource(null);
 });
 
