82752021bddf1625498cb5678f02f66bd1429737
Danny Koppenhagen <Danny.Koppenhagen@deutschebahn.com>
2023-07-25 21:07:36 +0200

    fix(ol-zone-control): define props with default values as optional

--- src/components/mapControls/OlZoneControl.vue
+++ src/components/mapControls/OlZoneControl.vue
@@ -9,7 +9,11 @@ import usePropsAsObjectProperties from "@/composables/usePropsAsObjectProperties
 
 const props = withDefaults(
   defineProps<
-    Omit<Options, "className" | "zone"> & { className: string; zones: Zone[] }
+    Omit<Options, "zone"> & {
+      className?: string;
+      centerOnClick?: boolean;
+      zones: Zone[];
+    }
   >(),
   {
     className: "ol-mapzone",
