7799442e2d324ebe411689152ec91e1f62b03daa
wendell <wendell@ilikecarrots.com>
2024-03-03 20:03:26 -0500

    together in one demo, v1

--- src/demos/WebglVectorLayerDemo.vue
+++ src/demos/WebglVectorLayerDemo.vue
@@ -16,20 +16,43 @@
       <ol-source-osm crossOrigin="anonymous" />
     </ol-tile-layer>
 
+    <!-- webgl points layer -->
+
+    <ol-webgl-vector-layer :styles="webglPointStyle">
+      <ol-source-webglvector
+        :format="geoJson"
+        crossOrigin="anonymous"
+        url="https://openlayers.org/en/latest/examples/data/geojson/point-samples.geojson"
+      />
+    </ol-webgl-vector-layer>
+
+    <!-- webgl lines layer -->
+
     <ol-webgl-vector-layer :styles="webglLineStyle">
       <ol-source-webglvector
         :format="geoJson"
         crossOrigin="anonymous"
-        url="https://openlayers.org/en/latest/examples/data/geojson/switzerland.geojson"
+        url="https://openlayers.org/en/latest/examples/data/geojson/line-samples.geojson"
       />
     </ol-webgl-vector-layer>
+
+    <!-- webgl polygons layer -->
+
+    <ol-webgl-vector-layer :styles="webglPolyStyle">
+      <ol-source-webglvector
+        :format="geoJson"
+        crossOrigin="anonymous"
+        url="https://openlayers.org/en/latest/examples/data/geojson/polygon-samples.geojson"
+      />
+    </ol-webgl-vector-layer>
+
   </ol-map>
 </template>
 
 <script setup>
 import { ref, inject } from "vue";
 
-const center = ref( [8.43, 46.82] );
+const center = ref( [ -73.3, 48.0] );
 const projection = ref("EPSG:4326");
 const zoom = ref(7);
 const rotation = ref(0);
@@ -37,9 +60,23 @@ const rotation = ref(0);
 const format = inject("ol-format");
 const geoJson = new format.GeoJSON();
 
+const webglPointStyle = {
+    'shape-points': 3,
+    'shape-radius': 10,
+    'shape-opacity': 0.5,
+    'shape-fill-color': 'blue',
+};
+
 const webglLineStyle = {
-    'stroke-width': 10,
+    'stroke-width': 5,
     'stroke-color': 'rgba(24,86,34,0.7)',
 };
+
+const webglPolyStyle = {
+    'stroke-width': 5,
+    'stroke-color': 'rgba(24,86,34,0.4)',
+    'fill-color': '#0000ff40',
+};
+
 </script>
 
