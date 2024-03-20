980148129c8abba05202fb376d9839a038a38406
Danny Koppenhagen <Danny.Koppenhagen@deutschebahn.com>
2024-03-20 14:41:25 +0100

    tests(e2e): provide tests for draw interaction

--- tests/MapPage.ts
+++ tests/MapPage.ts
@@ -1,5 +1,7 @@
 import { expect, Locator, Page } from "@playwright/test";
 
+type Point = [x: number, y: number];
+
 export class MapPage {
   readonly getStartedLink: Locator;
   readonly gettingStartedHeader: Locator;
@@ -42,5 +44,54 @@ export class MapPage {
 
   async canvasBBox() {
     return await this.page.locator("canvas").boundingBox();
+  }
+
+  async drawPoint(point: Point, dblClick = false) {
+    const boundingBox = await this.canvasBBox();
+
+    if (boundingBox) {
+      dblClick
+        ? await this.page.locator("canvas").dblclick({
+            position: {
+              x: point[0],
+              y: point[1],
+            },
+          })
+        : await this.page.locator("canvas").click({
+            position: {
+              x: point[0],
+              y: point[1],
+            },
+          });
+    } else {
+      console.error("Canvas element not found or not visible");
+    }
+  }
+
+  async drawLineString(...points: Point[]) {
+    for (let index = 0; index < points.length; index++) {
+      const last = index === points.length - 1;
+      await this.drawPoint(points[index], last);
+    }
+  }
+
+  async drawLine(pointA: Point, pointB: Point) {
+    await this.drawLineString(pointA, pointB);
+  }
+
+  async drawCircle(inner: Point, outer: Point) {
+    await this.drawPoint(inner);
+    await this.drawPoint(outer);
+  }
+
+  async drawOpenPolygon(...points: Point[]) {
+    for (let index = 0; index < points.length; index++) {
+      await this.drawPoint(points[index]);
+    }
+  }
+
+  async drawPolygon(...points: Point[]) {
+    await this.drawOpenPolygon(...points);
+    await this.drawPoint(points[0], true);
   }
 }
---
+++ tests/interactions.test.ts
@@ -0,0 +1,47 @@
+import { test } from "@playwright/test";
+
+import { MapPage } from "./MapPage";
+
+test.describe("ol-interaction-draw", () => {
+  test("should draw a Point Feature", async ({ page }) => {
+    const map = new MapPage(page);
+    await map.goto("/componentsguide/interactions/draw/");
+    await map.waitUntilReady();
+    await map.waitUntilCanvasLoaded();
+    await map.page.getByLabel("Geometry Type").selectOption("Point");
+    await map.drawPoint([150, 40]);
+    await map.drawPoint([120, 80]);
+    await map.drawPoint([200, 110]);
+    await map.checkCanvasScreenshot();
+  });
+
+  test("should draw a LineString Feature", async ({ page }) => {
+    const map = new MapPage(page);
+    await map.goto("/componentsguide/interactions/draw/");
+    await map.waitUntilReady();
+    await map.waitUntilCanvasLoaded();
+    await map.page.getByLabel("Geometry Type").selectOption("LineString");
+    await map.drawLineString([120, 80], [200, 110], [300, 180], [150, 20]);
+    await map.checkCanvasScreenshot();
+  });
+
+  test("should draw a Polygon Feature", async ({ page }) => {
+    const map = new MapPage(page);
+    await map.goto("/componentsguide/interactions/draw/");
+    await map.waitUntilReady();
+    await map.waitUntilCanvasLoaded();
+    await map.page.getByLabel("Geometry Type").selectOption("LineString");
+    await map.drawPolygon([120, 80], [200, 110], [300, 180], [200, 20]);
+    await map.checkCanvasScreenshot();
+  });
+
+  test("should draw a Circle Feature", async ({ page }) => {
+    const map = new MapPage(page);
+    await map.goto("/componentsguide/interactions/draw/");
+    await map.waitUntilReady();
+    await map.waitUntilCanvasLoaded();
+    await map.page.getByLabel("Geometry Type").selectOption("Circle");
+    await map.drawCircle([200, 200], [180, 180]);
+    await map.checkCanvasScreenshot();
+  });
+});
