65d970a3c42fc7195f2fde2e7c9f5a94cfad396a
Danny Koppenhagen <Danny.Koppenhagen@deutschebahn.com>
2024-04-19 23:20:07 +0200

    feat(ol-style-icon): allow to place icon content in slot
    closes #295

--- src/components/styles/OlStyleIcon.vue
+++ src/components/styles/OlStyleIcon.vue
@@ -1,14 +1,29 @@
 <template>
-  <div v-if="false"></div>
+  <div
+    ref="htmlContent"
+    v-if="!srcImageUrl"
+    style="display: flex; position: absolute; z-index: -1000"
+  >
+    <slot />
+  </div>
 </template>
 <script setup lang="ts">
 import Icon, { type Options } from "ol/style/Icon";
 import type { Ref } from "vue";
-import { inject, watch, onMounted, onUnmounted, computed } from "vue";
+import {
+  inject,
+  watch,
+  onMounted,
+  onUnmounted,
+  ref,
+  useSlots,
+  computed,
+} from "vue";
 import type Style from "ol/style/Style";
 import type Draw from "ol/interaction/Draw";
 import type Modify from "ol/interaction/Modify";
 import usePropsAsObjectProperties from "@/composables/usePropsAsObjectProperties";
+import domtoimage from "dom-to-image-more";
 
 const props = withDefaults(defineProps<Options>(), {
   anchorOrigin: "top-left",
@@ -21,6 +36,11 @@ const props = withDefaults(defineProps<Options>(), {
   rotation: 0,
 });
 
+const slots = useSlots();
+
+const htmlContent = ref<HTMLElement>();
+const srcImageUrl = ref<string>("");
+
 const style = inject<Ref<Style | null> | null>("style", null);
 const styledObj = inject<Ref<Draw | Modify | Style | null> | null>(
   "styledObj",
@@ -30,7 +50,10 @@ const styledObj = inject<Ref<Draw | Modify | Style | null> | null>(
 const properties = usePropsAsObjectProperties(props);
 
 const icon = computed(() => {
-  const ic = new Icon(properties);
+  const ic = new Icon({
+    ...properties,
+    src: properties.src || srcImageUrl.value,
+  });
   ic.load();
   return ic;
 });
@@ -47,15 +70,19 @@ watch(properties, () => {
   applyStyle();
 });
 
-watch(
-  () => style?.value,
-  () => {
-    applyStyle();
-  },
-);
+watch(style, () => {
+  applyStyle();
+});
 
-onMounted(() => {
-  style?.value?.setImage(icon.value);
+onMounted(async () => {
+  if (slots.default && htmlContent.value) {
+    srcImageUrl.value = await domtoimage.toSvg(htmlContent.value, {
+      width: htmlContent.value.offsetWidth,
+      height: htmlContent.value.offsetHeight,
+      copyDefaultStyles: true,
+    });
+  }
+  applyStyle();
 });
 
 onUnmounted(() => {
