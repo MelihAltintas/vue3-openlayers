c1d050f3dd077e9e13d08e2161f2029456313eec
Danny Koppenhagen <Danny.Koppenhagen@deutschebahn.com>
2025-05-03 07:29:59 +0200

    feat(ol-style-text): support all ol props
    closes #411

--- src/components/styles/OlStyleText.vue
+++ src/components/styles/OlStyleText.vue
@@ -5,38 +5,18 @@
 </template>
 
 <script setup lang="ts">
-import Text, { type Options, type TextPlacement } from "ol/style/Text";
+import Text, { type Options } from "ol/style/Text";
 import Fill from "ol/style/Fill";
-import Stroke, { type Options as StrokeOptions } from "ol/style/Stroke";
+import Stroke from "ol/style/Stroke";
 import type { Ref } from "vue";
 import { inject, watch, onMounted, onUnmounted, provide, computed } from "vue";
 import type Style from "ol/style/Style";
 import type Draw from "ol/interaction/Draw";
 import type Modify from "ol/interaction/Modify";
 import usePropsAsObjectProperties from "@/composables/usePropsAsObjectProperties";
-import type { Color } from "ol/color";
-import type { ColorLike } from "ol/colorlike";
 import { Interaction } from "ol/interaction";
 
-const props = defineProps<{
-  font?: string;
-  maxAngle?: number;
-  offsetX?: number;
-  offsetY?: number;
-  overflow?: boolean;
-  placement?: TextPlacement;
-  scale?: number;
-  rotateWithView?: boolean;
-  rotation?: number;
-  text?: string;
-  textAlign?: CanvasTextAlign;
-  textBaseline?: CanvasTextBaseline;
-  padding?: [number, number, number, number];
-  fill?: Color | ColorLike;
-  stroke?: StrokeOptions;
-  backgroundFill?: Color | ColorLike;
-  backgroundStroke?: StrokeOptions;
-}>();
+const props = defineProps<Options>();
 
 const style = inject<Ref<Style | null> | null>("style", null);
 const styledObj = inject<Ref<Draw | Modify | Style | null> | null>(
